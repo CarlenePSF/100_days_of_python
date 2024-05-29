d73db317182332aa13fd7467c60c4820ccf399b3
carlene <carlenepaula@gmail.com>
2024-05-29 18:49:13 -0300

    first_commit

---
+++ day_01/day_01_0.py
@@ -0,0 +1,29 @@
+"""
+# a new line with \n
+print("Hello, World!\nHello, World!\nHello, World!")
+
+# concatenate strings
+print("Hello"+" "+"Angela")
+
+# input data
+print("Hello " + input("What is your name?"))
+
+# Try it yourself. Print the input text into the output area.
+print("What is your name?")
+print(input(), "\n")
+
+print("Enter a number:")
+num1 = int(input())
+print(num1)
+
+print("Enter another number:")
+num2 = int(input())
+print(num2)
+print("The sum of the two numbers is", num1 + num2)
+
+
+print("The word has", len(input("Write a word: ")), "letters.")
+"""
+time_until_midnight = "5"
+
+print("There are " + time_until_Minight + " hours until midnight")
---
+++ day_01/day_01_1.py
@@ -0,0 +1,8 @@
+"""
+Project Day 1 - Band name Generation
+"""
+
+print("Welcome to the Band Name Generator program!\n")
+city = input("What's the name of the city you grew up? ")
+pet = input("What's your pet's name? ")
+print("Your band name is", city + pet)
