7ec5cc06f934bbf14df9e3ee3fe440f7190034d2
carlene <carlenepaula@gmail.com>
2024-06-04 11:13:08 -0300

    init_day_7

---
+++ day_06/main.py
@@ -0,0 +1,27 @@
+"""
+Code blocks and indentation, functions and While loops
+
+for indentation see PEP8:  https://peps.python.org/pep-0008/
+
+def my_function():
+    # do this
+    # Then do that
+    # finally, do that
+
+
+using function in Reeborg
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+"""
+
+
+def my_function():
+    print("Hello!")
+    print("Bye!")
+
+
+my_function()
+
+
+
+
+
