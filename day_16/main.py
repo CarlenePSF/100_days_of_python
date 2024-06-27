7a8fb18a38179994f1487d1fb7dfd9e0901bbf63
carlene <carlenepaula@gmail.com>
2024-06-27 15:22:35 -0300

    finishing_day_14

---
+++ day_16/main.py
@@ -0,0 +1,43 @@
+"""
+Object-oriented programming
+
+class - waiter
+
+objects - can be multiple
+our waiters are :
+    janny
+    jack
+
+
+attributes - what it has - "variables"
+    is_holding_plate = True
+    tables_responsible = [4, 5, 6]
+
+methods - what it does - "functions"
+    take_order(table, order)
+
+    take_payment(table, payment)
+
+
+# Example
+class WaiterBlueprint():
+    ...
+
+from waiter import Waiter
+
+jack = WaiterBlueprint()
+"""
+
+from turtle import Turtle
+
+joy = Turtle()
+print(joy)
+
+
+
+
+
+
+
+
+
