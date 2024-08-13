34874695aa569ff5936345c867814aa2fe3f69e0
carlene <carlenepaula@gmail.com>
2024-08-13 12:30:47 -0300

    update_data_day17

--- day_17/quiz-game-start/data.py
+++ day_17/quiz-game-start/data.py
@@ -43,35 +43,40 @@ New question can be retrieved from https://opentdb.com/
 # ]
 
 question_data = [
-    {"type": "boolean",
+    {
+     "type": "boolean",
      "difficulty": "easy",
      "category": "Science: Computers",
      "question": "The programming language Python is based off a modified version of JavaScript.",
      "correct_answer": "False",
      "incorrect_answers": ["True"]
      },
-     {"type": "boolean",
+     {
+      "type": "boolean",
       "difficulty": "easy",
       "category": "History",
       "question": "The Cold War ended with Joseph Stalin's death.",
       "correct_answer": "False",
       "incorrect_answers": ["True"]
       },
-     {"type": "boolean",
+     {
+      "type": "boolean",
       "difficulty": "easy",
       "category": "Science: Computers",
       "question": "Linus Torvalds created Linux and Git.",
       "correct_answer": "True",
       "incorrect_answers": ["False"]
       },
-    {"type": "boolean",
+    {
+     "type": "boolean",
      "difficulty": "easy",
      "category": "Science: Computers",
      "question": "RAM stands for Random Access Memory.",
      "correct_answer": "True",
      "incorrect_answers": ["False"]
      },
-    {"type": "boolean",
+    {
+     "type": "boolean",
      "difficulty": "easy",
      "category": "History",
      "question": "In World War ll, Great Britian used inflatable tanks on the ports of Great Britain"
@@ -79,35 +84,40 @@ question_data = [
      "correct_answer": "True",
      "incorrect_answers": ["False"]
      },
-    {"type": "boolean",
+    {
+     "type": "boolean",
      "difficulty": "easy",
      "category": "Celebrities",
      "question": "Tupac Shakur died due to complications from being stabbed in 1996.",
      "correct_answer": "False",
      "incorrect_answers": ["True"]
      },
-    {"type": "boolean",
+    {
+     "type": "boolean",
      "difficulty": "easy",
      "category": "Science: Gadgets",
      "question": "Microphones can be used not only to pick up sound, but also to project sound similar to a speaker.",
      "correct_answer": "True",
      "incorrect_answers": ["False"]
      },
-    {"type": "boolean",
+    {
+     "type": "boolean",
      "difficulty": "easy",
      "category": "Entertainment: Television",
      "question": "In Battlestar Galactica (2004), Cylons were created by man as cybernetic workers and soldiers.",
      "correct_answer": "True",
      "incorrect_answers": ["False"]
      },
-    {"type": "boolean",
+    {
+     "type": "boolean",
      "difficulty": "easy",
      "category": "Geography",
      "question": "There is a city called Rome in every continent on Earth.",
      "correct_answer": "False",
      "incorrect_answers": ["True"]
      },
-    {"type": "boolean",
+    {
+     "type": "boolean",
      "difficulty": "easy",
      "category": "Entertainment: Video Games",
      "question": "In the Super Mario series, the character Pauline is a Princess like Princess Peach.",
