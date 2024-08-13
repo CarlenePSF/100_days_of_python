3db6ad889b2e9e1ffd4ce0542def7b557bc62f49
carlene <carlenepaula@gmail.com>
2024-08-13 10:06:54 -0300

    init_day_17

--- day_17/main.py
+++ day_17/main.py
@@ -4,9 +4,25 @@ class User:
         self.name = user_name
         self.id_number = id_number
         self.followers = 0 # default value
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user_1 = User("Angela", "001")
-print(f'user name: {user_1.name}, id {user_1.id_number} and number of followers {user_1.followers}')
 user_2 = User("Bob", "002")
-print(user_2.name,  user_1.id_number)
+
+print(f'user name: {user_1.name},'
+      f'id {user_1.id_number},'
+      f' number of followers {user_1.followers}'
+      f' and following {user_1.following}')
+
+# call a method
+user_1.follow(user_2)
+
+print(f'user name: {user_1.name},'
+      f'id {user_1.id_number},'
+      f' number of followers {user_1.followers}'
+      f' and following {user_1.following}')
---
+++ day_17/quiz-game-start/question_model.py
@@ -0,0 +1,14 @@
+"""
+Attributes:
+    text
+    answer
+new_q = Question("some text", "True or false")
+"""
+
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+
+new_q = Question("2+3=5", "True")
