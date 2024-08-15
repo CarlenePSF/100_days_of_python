5a8af112a3d4d3603771b0a25f5c88b9c854d832
carlene <carlenepaula@gmail.com>
2024-08-15 15:32:25 -0300

    mais_exercicios_poo

---
+++ exercices in oop/sistema_de_biblioteca/data.py
@@ -0,0 +1,12 @@
+acervo_livros = [
+    {
+        'titulo': 'Python programming: An introduction to computer science',
+        'autor': 'John Zelle',
+        'paginas': '554'
+    },
+    {
+        'titulo': 'Designing Machine Learning Systems: An Interative Process for Production-Ready Applications',
+        'autor': 'Chip Huyen',
+        'paginas': '389'
+    }
+]
