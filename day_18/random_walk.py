d7cd9799edd9baf76804f112e06ed70fbf8590ea
carlenepsf <carlenepaula@gmail.com>
2024-10-20 16:22:38 -0300

    day_18_random_walk

---
+++ day_18/random_walk.py
@@ -0,0 +1,59 @@
+########### Challenge 4 - Random Walk ########
+
+import turtle
+from random import randint, random, choice
+from turtle import Turtle, Screen
+turtle.colormode(255)
+t = Turtle()
+t.pensize(7)
+t.speed("fastest")
+t.screen.title('random-walk')
+
+screen = Screen()
+
+def random_color():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    return r, g, b
+
+def move_up_or_down(p_y):
+    if p_y > p_up:
+        # para baixo
+        t.setheading(270)
+    else:
+        # para cima
+        t.setheading(90)
+    t.forward(30)
+
+
+def move_right_or_left(p_x):
+    if p_x > p_right:
+        # para esquerda
+        t.setheading(0)
+    else:
+        # para direita
+        t.setheading(180)
+    t.forward(30)
+
+
+steps = 100
+i = 0
+j = 0
+p_up = 5
+p_right = 5
+
+while i < steps:
+    while j < steps:
+        py = randint(1, 10)
+        px = randint(1, 10)
+        t.pencolor(random_color())
+
+        move_up_or_down(py)
+        move_right_or_left(px)
+        j += 1
+    i += 1
+
+screen.mainloop()
+
+
---
+++ day_18/turtle_challenge.py
@@ -0,0 +1,50 @@
+from random import randint, random, choice
+from turtle import Turtle, Screen
+
+t = Turtle()
+screen = Screen()
+
+t.screen.title('Turtle Challenge')
+
+colours = [
+    "CornflowerBlue", "DarkOrchid",
+    "IndianRed", "DeepSkyBlue",
+    "LightSeaGreen", "wheat",
+    "SlateGray", "SeaGreen"
+]
+
+########### Challenge 1 - Draw a square ########
+for _ in range(4):
+    t.forward(100)
+    t.left(90)
+
+########### Challenge 2 - Draw a Dashed Line ########
+
+t.color("blue")
+for _ in range(15):
+    t.forward(10)
+    t.penup()
+    t.forward(10)
+    t.pendown()
+
+########### Challenge 3 - Draw Shapes ########
+
+def form(number_of_sides):
+    for _ in range(number_of_sides):
+        angle = 360 / number_of_sides
+        t.forward(100)
+        t.right(angle)
+
+
+i = 3
+while i < 11:
+    t.color(choice(colours))
+    form(i)
+    i += 1
+
+screen.mainloop()
+
+
+
+
+
